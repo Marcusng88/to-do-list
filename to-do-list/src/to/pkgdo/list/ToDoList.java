6a0af3eaf5b01a6beaf250bd7ce7cbf8fdec8478
Marcus Ng <ngzhengjie888@gmail.com>
2024-11-12 10:54:14 +0800

    added hello wwww

--- to-do-list/src/to/pkgdo/list/ToDoList.java
+++ to-do-list/src/to/pkgdo/list/ToDoList.java
@@ -14,7 +14,7 @@ public class ToDoList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello wwwwww");
         // TODO code application logic here
     }
     
